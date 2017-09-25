424ede6c8414cc655100be723825f0621790b63d
David White <dwhite@oteemo.com>
2017-09-25 18:21:44 -0400

    Format change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <h1>Sample PHP</h1>
 <p>This is my PHP project</p>
 <p>There are <strong>many</strong> like it...</p>
-<p>...but this one is mine.</p>
+<p>...but this <en>one</en> is mine.</p>
 
 <?php
 echo (new \DateTime())->format('Y-m-d H:i:s');

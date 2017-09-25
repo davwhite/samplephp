1dd11eb351244ec7f321973dce067f28529ad1ba
David White <dwhite@oteemo.com>
2017-09-25 10:20:36 -0400

    Added date printout.

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<?php
-?>
 
 <h1>Sample PHP</h1>
 <p>This is my PHP project</p>
 <p>There are many like it...</p>
 <p>...but this one is mine.</p>
+
+<?php
+echo (new \DateTime())->format('Y-m-d H:i:s');
+?>
